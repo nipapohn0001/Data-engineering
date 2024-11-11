c56c1b59a045633bf70b0d60a959163ced9c0c8c
Prim18 <6431501008@lamduan.mfu.ac.th>
2024-11-12 01:19:11 +0700

    up6

--- js/book-list2.js
+++
@@ -1,6 +0,0 @@
-// ฟังก์ชันเพื่อแสดงรายละเอียดหนังสือ (ตัวอย่าง)
-function viewBookDetails(bookId) {
-    // คุณสามารถแทนที่การแสดงรายละเอียดหนังสือได้ตามต้องการ
-    alert("Viewing details for book ID: " + bookId);
-  }
-  
